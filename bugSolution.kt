a3c42f63cf13be5d4303345f08224a4945fae6e4
BugHunter-X <askzenai@gmail.com>
2025-01-19 04:20:07 +0100

    Add bugSolution.kt file

---
+++ bugSolution.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val list = mutableListOf<Int>(1, 2, 3, 4, 5)
+    list.removeIf { it > 2 }
+    println(list) // Output: [1, 2]
+
+    val set = mutableSetOf<Int>(1, 2, 3, 4, 5)
+    set.removeIf { it > 2 }
+    println(set) // Output: [1, 2]
+
+    val map = mutableMapOf<Int, String>(1 to "One", 2 to "Two", 3 to "Three", 4 to "Four", 5 to "Five")
+    map.removeIf { it.key > 2 }
+    println(map) //Output: {1=One, 2=Two}
+} 
